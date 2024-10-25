239afe47da5082a45ad5e0d91313f5e3e38a5e99
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-25 12:47:55 +0530

    changed the files in canvas

--- app/components/canvas/Stars.tsx
+++ app/components/canvas/Stars.tsx
@@ -1,16 +1,18 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+import { inSphere } from "maath/random";
+import * as THREE from 'three';
 
 interface StarsProps {
-  // Add any props that you may want to pass to the Stars component
-  [key: string]: any; // For flexibility, you can specify specific props instead of using `any`
+  [key: string]: any; // Allows flexibility in props
 }
 
 const Stars: React.FC<StarsProps> = (props) => {
   const ref = useRef<THREE.Points>(null);
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState<Float32Array>(() => {
+    return inSphere(new Float32Array(5000), { radius: 1.2 }) as Float32Array;
+  });
 
   useFrame((state, delta) => {
     if (ref.current) {
@@ -24,7 +26,7 @@ const Stars: React.FC<StarsProps> = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#f272c8'
+          color="#f272c8"
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
@@ -36,7 +38,7 @@ const Stars: React.FC<StarsProps> = (props) => {
 
 const StarsCanvas: React.FC = () => {
   return (
-    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
