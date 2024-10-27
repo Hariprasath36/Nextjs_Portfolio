45290fb4868f0daac5d3d868e817c23e67338c8b
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-27 08:31:29 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,35 @@
-// app/layout.tsx
 import type { Metadata } from "next";
-import "./globals.css"; // Keep your global styles including Poppins import
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Your Portfolio Title",
-  description: "Description of your portfolio",
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body className="antialiased">
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,30 @@
-// app/page.tsx
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+"use client";
+
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        <Navbar />
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className='relative z-0'>
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
+    </main>
   );
 };
 
