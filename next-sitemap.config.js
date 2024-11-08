b4784629f9a7679ca82e6fcd12ead6ec403332c7
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-08 13:48:27 +0530

    1:27:11

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
-// next-sitemap.config.js
-module.exports = {
-    siteUrl: 'https://its-hariprasath-portfolio.vercel.app/', // Replace with your actual domain
-    generateRobotsTxt: true, // Generates robots.txt file
-  };
-  
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://its-hariprasath-portfolio.vercel.app/', // Replace with your actual domain
+  generateRobotsTxt: true, // Generates robots.txt file
+};
+
+export default config;
